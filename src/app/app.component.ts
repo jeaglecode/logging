aee4e2b2053885800ad23ae98e03076cde7d70f7
jeaglecode <jesserduenas@yahoo.com>
2023-04-24 20:35:04 -0700

    initial push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,6 @@ import {Component, OnInit} from '@angular/core';
 // import { AuthService } from '@auth0/auth0-angular';
 import { AuthService } from './auth.service';
 
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
